3dc88ea1afaadee02b3a857d91ce96c0db10954b
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-07-15 23:55:46 +0400

    adding click textDecoration/jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Todos from './Todos/Todos';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, text: 'Learn React', completed: false },
     { id: 2, text: 'Build a React app', completed: true },
     { id: 3, text: 'Explore advanced React topics', completed: true },
-  ];
+  ]);
+
+  function addDecoration(id) {
+    const newTodo = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(newTodo);
+  }
 
   return (
     <div>
-      <Todos todoList={todos} />
+      <Todos todoList={todos} addDecoration={addDecoration} />
     </div>
   );
 }
--- src/Todos/Todos.jsx
+++ src/Todos/Todos.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import style from './Todos.module.scss';
 
 function Todos(props) {
-  const todos = [props.todoList];
-
+  const todos = [props.todoList][0];
+  console.log(todos);
   return (
     <div className={style.container}>
       <h1>Todo List</h1>
       <ul>
-        {todos[0].map((todo) => {
+        {todos.map((todo) => {
           return (
             <li
               key={todo.id}
-              className={
-                todo.completed ? `${style.completed}` : ``
-              }
+              className={todo.completed ? `${style.completed}` : ``}
+              onClick={() => props.addDecoration(todo.id)}
             >
               {todo.text}
             </li>
